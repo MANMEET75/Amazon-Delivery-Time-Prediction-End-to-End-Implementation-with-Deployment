e50bbcd310c27afe695c25159a36fd4fe1ce9fb8
MANMEET SINGH (AWM) <97391884+MANMEET75@users.noreply.github.com>
2023-05-21 14:38:24 +0530

    Update application.py

--- application.py
+++ application.py
@@ -48,7 +48,8 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
-
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0",debug=True)        
+if __name__ == '__main__':
+    app.run_server(host="0.0.0.0")
 
